76c0a4a8694a4f01edcb2079558d615555bf923f
Alalani-school <alalani@bayviewglen.ca>
2026-06-27 09:05:22 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import http from "http";
+import { WebSocketServer } from "ws";
+const app = express();
+const server = http.createServer(app);
+app.use(express.static("public"));
+const wss = new WebSocketServer({ server });
+wss.on("connection", (ws) => {
+  console.log("client connected");
+  ws.send("connected to server");
+  ws.on("message", (message) => {
+    console.log("received:", message.toString());
+    ws.send(message);
+  });
+});
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`server running on port ${PORT}`);
+});
