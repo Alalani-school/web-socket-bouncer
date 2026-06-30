ab1175e2cda85d3de2b3a9a93901643d65ff299b
Alalani-school <alalani@bayviewglen.ca>
2026-06-29 20:20:21 -0400

    Enhance WebSocket connection handling and welcome message
    Added a welcome message for new WebSocket connections and improved message handling logic.

--- server.js
+++ server.js
@@ -3,18 +3,33 @@ import http from "http";
 import { WebSocketServer } from "ws";
 const app = express();
 const server = http.createServer(app);
+let now = new Date();
+const welcomeMessage = {
+  "TYPE":"WELCOME",
+  "MESSAGE":"Sucessfully connected to server",
+  "DATE":now
+}
+let onlineUsers = [];
 app.use(express.static("public"));
 const wss = new WebSocketServer({ server });
 wss.on("connection", (ws) => {
   console.log("client connected");
-  ws.send("connected to server");
+  ws.send(JSON.stringify(welcomeMessage));
   ws.on("message", (message) => {
     console.log("received:", message.toString());
-    if(!message === "[PING]") {
+    if(message !== "[PING]") {
+      let jsonMessage = JSON.parse(message.toString());
+      if(jsonMessage.TYPE === "LOGON") onlineUsers.push(jsonMessage.MESSAGE);
+      else if(jsonMessage.TYPE == "LOGOFF") if(onlineUsers.indexOf(jsonMessage.MESSAGE) > -1) onlineUsers.splice(onlineUsers.indexOf(jsonMessage.MESSAGE), 1);
+      let Data = {
+        "TYPE":"ONLINE",
+        "MESSAGE":onlineUsers
+      }
+      for(const client of wss.clients) if (client.readyState === client.OPEN) client.send(JSON.stringify(Data));
+    }
+    else {
       for (const client of wss.clients) {
-        if (client.readyState === client.OPEN && client !== ws) {
-          client.send(message.toString());
-        }
+        if (client.readyState === client.OPEN && client !== ws) client.send(message.toString());
       }
     }
   });
